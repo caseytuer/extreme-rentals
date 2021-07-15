49a213cb419845c9a8e6e4d51f498fe2469c5c93
Casey Tuer <caseytuer@gmail.com>
2021-07-15 12:48:30 -0700

    user logout functioning

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,4 +30,13 @@ router.post(
     }),
 );
 
+// logout
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
 module.exports = router;
