c00060c8c559f2df0ade557f4cde289efc856e39
Casey Tuer <caseytuer@gmail.com>
2021-07-15 10:55:39 -0700

    defined model scopes for User

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [4, 30],
+        len: [3, 30],
         isNotEmail(value) {
           if (Validator.isEmail(value)) {
             throw new Error('Username cannot be an email.');
@@ -29,9 +29,24 @@ module.exports = (sequelize, DataTypes) => {
         len: [60, 60]
       },
     },
-  }, {});
+  },
+    {
+      defaultScope: {
+        attributes: {
+          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ['hashedPassword'] },
+        },
+        loginUser: {
+          attributes: {},
+        },
+      },
+    });
   User.associate = function (models) {
-    // associations 
+    // associations
   };
   return User;
 };
